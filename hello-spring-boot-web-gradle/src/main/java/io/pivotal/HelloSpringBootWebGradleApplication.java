cf6f9b08787a32dbc010636c4570dd3bf20718d4
Vijay Vepakomma <vijay.vepakomma@philips.com>
2016-12-15 10:06:22 -0500

    hello-spring-boot-web-gradle

--- hello-spring-boot-web-gradle/src/main/java/io/pivotal/HelloSpringBootWebGradleApplication.java
+++ hello-spring-boot-web-gradle/src/main/java/io/pivotal/HelloSpringBootWebGradleApplication.java
@@ -2,11 +2,20 @@ package io.pivotal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloSpringBootWebGradleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringBootWebGradleApplication.class, args);
 	}
+
+	@RequestMapping("/")
+    public String helloWorld(){
+	    return  "Hello World!";
+    }
+
 }
